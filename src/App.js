e3861acf8ad3f5ecc1055d3303addf2d9556b141
ThaiTX <thaidzdz11@gmail.com>
2021-12-28 21:32:36 +0700

    Design Front-end

--- src/App.js
+++ src/App.js
@@ -1,9 +1,97 @@
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Tran Xuan Thai</h1>
+    <div className="Trelo">
+      <nav className="navbar app">App bar</nav>
+      <nav className="navbar board">Board bar</nav>
+      <div className="board-columns">
+        <div className="column">
+              <header className="">BrainStorm</header>
+              <ul>
+                <li> 
+                  <img src="https://scontent.fhan3-4.fna.fbcdn.net/v/t1.6435-9/190680881_1581029805433795_908095176575543913_n.jpg?_nc_cat=104&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=0NTb7GyWX2oAX9nwwkV&_nc_ht=scontent.fhan3-4.fna&oh=00_AT9EY_X4MsqPOI0HbREK2XUdyVohD2U-x74S9skPRn_Z1w&oe=61F0CC85"/>
+                  Title : ThaiTran 
+                </li>
+                <li>Day la title  </li>
+                <li>Day la title  </li>
+                <li>Day la title  </li>
+                <li>Day la title  </li>
+              </ul>
+              <footer>Add onther card </footer>
+        </div>
+        <div className="column">
+              <header className="">BrainStorm</header>
+              <ul>
+                <li> 
+                  <img src="https://scontent.fhan3-4.fna.fbcdn.net/v/t1.6435-9/190680881_1581029805433795_908095176575543913_n.jpg?_nc_cat=104&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=0NTb7GyWX2oAX9nwwkV&_nc_ht=scontent.fhan3-4.fna&oh=00_AT9EY_X4MsqPOI0HbREK2XUdyVohD2U-x74S9skPRn_Z1w&oe=61F0CC85"/>
+                  Title : ThaiTran 
+                </li>
+                <li>Day la title  </li>
+                <li>Day la title  </li>
+                <li>Day la title  </li>
+                <li>Day la title  </li>
+              </ul>
+              <footer>Add onther card </footer>
+        </div>
+        <div className="column">
+              <header className="">BrainStorm</header>
+              <ul>
+                <li> 
+                  <img src="https://scontent.fhan3-4.fna.fbcdn.net/v/t1.6435-9/190680881_1581029805433795_908095176575543913_n.jpg?_nc_cat=104&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=0NTb7GyWX2oAX9nwwkV&_nc_ht=scontent.fhan3-4.fna&oh=00_AT9EY_X4MsqPOI0HbREK2XUdyVohD2U-x74S9skPRn_Z1w&oe=61F0CC85"/>
+                  Title : ThaiTran 
+                </li>
+                <li>Day la title  </li>
+                <li>Day la title  </li>
+                <li>Day la title  </li>
+                <li>Day la title  </li>
+              </ul>
+              <footer>Add onther card </footer>
+        </div>
+        <div className="column">
+              <header className="">BrainStorm</header>
+              <ul>
+                <li> 
+                  <img src="https://scontent.fhan3-4.fna.fbcdn.net/v/t1.6435-9/190680881_1581029805433795_908095176575543913_n.jpg?_nc_cat=104&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=0NTb7GyWX2oAX9nwwkV&_nc_ht=scontent.fhan3-4.fna&oh=00_AT9EY_X4MsqPOI0HbREK2XUdyVohD2U-x74S9skPRn_Z1w&oe=61F0CC85"/>
+                  Title : ThaiTran 
+                </li>
+                <li>Day la title  </li>
+                <li>Day la title  </li>
+                <li>Day la title  </li>
+                <li>Day la title  </li>
+              </ul>
+              <footer>Add onther card </footer>
+        </div>
+        <div className="column">
+              <header className="">BrainStorm</header>
+              <ul>
+                <li> 
+                  <img src="https://scontent.fhan3-4.fna.fbcdn.net/v/t1.6435-9/190680881_1581029805433795_908095176575543913_n.jpg?_nc_cat=104&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=0NTb7GyWX2oAX9nwwkV&_nc_ht=scontent.fhan3-4.fna&oh=00_AT9EY_X4MsqPOI0HbREK2XUdyVohD2U-x74S9skPRn_Z1w&oe=61F0CC85"/>
+                  Title : ThaiTran 
+                </li>
+                <li>Day la title  </li>
+                <li>Day la title  </li>
+                <li>Day la title  </li>
+                <li>Day la title  </li>
+              </ul>
+              <footer>Add onther card </footer>
+        </div>
+        <div className="column">
+              <header className="">BrainStorm</header>
+              <ul>
+                <li> 
+                  <img src="https://scontent.fhan3-4.fna.fbcdn.net/v/t1.6435-9/190680881_1581029805433795_908095176575543913_n.jpg?_nc_cat=104&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=0NTb7GyWX2oAX9nwwkV&_nc_ht=scontent.fhan3-4.fna&oh=00_AT9EY_X4MsqPOI0HbREK2XUdyVohD2U-x74S9skPRn_Z1w&oe=61F0CC85"/>
+                  Title : ThaiTran 
+                </li>
+                <li>Day la title  </li>
+                <li>Day la title  </li>
+                <li>Day la title  </li>
+                <li>Day la title  </li>
+              </ul>
+              <footer>Add onther card </footer>
+        </div>
+      </div>
+
     </div>
   );
 }
